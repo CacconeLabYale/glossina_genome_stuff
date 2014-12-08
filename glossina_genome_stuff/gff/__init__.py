ffeb9104c7c830ee7085084eb66239da06b07b00
Augustine Dunn <wadunn83@gmail.com>
2014-12-08 15:39:30 -0500

    2014-12-08T18:56:15Z

---
+++ glossina_genome_stuff/gff/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py is part of the 'glossina_genome_stuff' package.
+# It was written by Gus Dunn and was created on 12/3/14.
+# 
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+__init__.py
+=================================================
+Purpose:
+
+"""
+__author__ = 'Gus Dunn'
+
---
+++ glossina_genome_stuff/gff/chromosomes.py
@@ -0,0 +1,14 @@
+# chromosomes is part of the 'glossina_genome_stuff' package.
+# It was written by Gus Dunn and was created on 12/3/14.
+# 
+# Please see the license info in the root folder of this package.
+
+"""
+=================================================
+chromosomes
+=================================================
+Purpose:
+
+"""
+__author__ = 'Gus Dunn'
+
